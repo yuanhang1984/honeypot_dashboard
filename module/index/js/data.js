4dd4dc2240364e588955e50e9343d4ec96fac8e9
Satoshi Nakamoto <SatoshiNakamoto@btc.com>
2021-10-30 19:20:21 +0800

    2021-10-30 19:20:21

--- module/index/js/data.js
+++ module/index/js/data.js
@@ -1,1 +1,1 @@
-let dataSource ={"chart_data":[{"loss_value":0,"datetime":"2021-10-30 19:09:55","profit_value":0},{"loss_value":0,"datetime":"2021-10-30 19:13:59","profit_value":0}],"loss_amount":"0","block_datetime":"2021-10-29 18:42:21"};
+let dataSource ={"chart_data":[{"loss_value":0,"datetime":"2021-10-30 19:09:55","profit_value":0},{"loss_value":0,"datetime":"2021-10-30 19:13:59","profit_value":0},{"loss_value":0,"datetime":"2021-10-30 19:19:04","profit_value":0}],"loss_amount":"0","block_datetime":"2021-10-29 18:42:21"};
